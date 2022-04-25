b98aaefdab16e0a8dcaea85211088a33c0392b59
Anas-Nady <eng.anas.nady@gmail.com>
2022-04-25 07:28:57 +0200

    Add overlay it click to image

--- js/main.js
+++ js/main.js
@@ -1,26 +1,60 @@
 
-$(function(){
+// Create Poup with The Image 
 
-    $('body').css("paddingTop", $(".navbar").innerHeight())
+let ourGallery = document.querySelectorAll('.row > div > img');
 
-});
+ourGallery.forEach(img => {
 
+    img.addEventListener('click', (e) => {
 
-// btn Scroll 
-window.onscroll = function() {
+        // Create overlay Element
+        let overlay = document.createElement('div');
 
-    if (window.pageYOffset >= 800) {
-    btnScroll.style.display = 'block'
-    }
-    else{
-    btnScroll.style.display = 'none'
-    }
+        // Add Class to Overlay 
+        overlay.className = 'Popup-overlay';
 
-}
+        // Append Overlay To body 
+        document.body.appendChild(overlay);
 
-btnScroll.onclick = function() {
-    window.scrollTo({
-        top:0,
-        left:0,
+        // Create The Popup Box 
+        let popupBox = document.createElement('div');
+
+        // Add Class To The popup Box
+        popupBox.className = 'popup-box';
+
+        // Create The Image
+        let popupImage = document.createElement('img');
+
+        // Set Image Src 
+        popupImage.src = img.src;
+
+        // Add Image To popup Box 
+        popupBox.appendChild(popupImage);
+
+        // Append The popup Box To Body 
+        document.body.appendChild(popupBox);
+
+        // Create The Close Span 
+        let closeButton = document.createElement('span');
+
+        // Create the Close Button Text 
+        let closeButtonText = document.createTextNode('X');
+
+        // Append Text To Close Button 
+        closeButton.appendChild(closeButtonText);
+
+        // Add Class To Close Button 
+        closeButton.className = 'close-button';
+
+        // Add Close Button To The popup Box
+        popupBox.appendChild(closeButton);
+
+        // Close Popup To Click Close 
+        closeButton.addEventListener('click', function() {
+            popupBox.style.display = 'none';
+            overlay.style.display = 'none';
+        })
+
     })
-}
+
+})
